6384e4253b1283194ccda40d2ecd9da9771e214d
Ramon Gebben <github@ra-ge.net>
2018-09-10 13:17:44 +0200

    Added build step

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /**
  * ellipsisify
  * @param {string} str The string you need to cut off
@@ -7,13 +8,14 @@
  * @param {string} [ellipsis='...'] The string to add to the end
  * @returns {string} ellipsisified string
  */
-
-function ellipsisify(str, cutoff, remain) {
-  var ellipsis = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '...';
+function ellipsisify(str, cutoff, remain, ellipsis = '...') {
   if (str.length <= cutoff) return str;
   if (!cutoff || cutoff + remain >= str.length) return str;
-  if (!remain) return "".concat(str.substr(0, cutoff)).concat(ellipsis);
-  return "".concat(str.substr(0, cutoff)).concat(ellipsis).concat(str.substr(str.length - remain));
+  if (!remain) return `${str.substr(0, cutoff)}${ellipsis}`;
+
+  return (
+    `${str.substr(0, cutoff)}${ellipsis}${str.substr(str.length - remain)}`
+  );
 }
 
 module.exports = ellipsisify;
